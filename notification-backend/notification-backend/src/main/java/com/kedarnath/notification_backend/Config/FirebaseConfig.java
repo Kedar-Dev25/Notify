25e904017065362948d691f1e9b90630f069ad23
Kedar-Dev25 <kedar.code7@gmail.com>
2026-06-26 16:25:26 +0530

    Added serviceAccountKey.json file as environment variable

--- notification-backend/notification-backend/src/main/java/com/kedarnath/notification_backend/Config/FirebaseConfig.java
+++ notification-backend/notification-backend/src/main/java/com/kedarnath/notification_backend/Config/FirebaseConfig.java
@@ -1,24 +1,13 @@
-package com.kedarnath.notification_backend.config;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Configuration;
-
-import java.io.InputStream;
-
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws Exception {
-        InputStream serviceAccount =
-                getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
 
-        if (serviceAccount == null) {
-            throw new RuntimeException("Firebase service account file not found in resources!");
-        }
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+
+        InputStream serviceAccount =
+                new ByteArrayInputStream(firebaseConfig.getBytes());
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -28,6 +17,6 @@ public class FirebaseConfig {
             FirebaseApp.initializeApp(options);
         }
 
-        System.out.println("Firebase initialized successfully 🚀");
+        System.out.println("Firebase initialized successfully");
     }
 }
